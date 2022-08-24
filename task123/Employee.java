5a811c5578dd184580bd2c4cba3d72b126449f38
Andrey0201 <andriutsa1@ukr.net>
2022-08-25 00:12:43 +0300

    edit

--- task123/Employee.java
+++ task123/Employee.java
@@ -2,13 +2,13 @@ package task123;
 
 public class Employee {
 
-    private String name;
+    private String fullName;
     private String email;
     private String phoneNumber;
     private int age;
 
-    public Employee(String name, String email, String phoneNumber, int age) {
-        this.name = name;
+    public Employee(String fullName, String email, String phoneNumber, int age) {
+        this.fullName = fullName;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.age = age;
